615799c46b6dbd6582e684a10eb77a26998bd9a0
Adriano Misina <adriano.misina@gmail.com>
2021-05-15 15:50:52 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,118 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated >
+      <q-toolbar class="bg-blue-grey-10">
+        <q-btn
+          flat
+          dense
+          round
+          icon="headphones"
+          aria-label="Menu"
+          @click="leftDrawerOpen =! leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Sound on Lofi
+        </q-toolbar-title>
+
+        <div>
+          <strong>Music for Life</strong>
+        </div>
+        
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      
+      content-class="bg-blue-grey-10"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-white"
+        >
+          <strong>Navegation</strong>  
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+          class="text-white"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
+      style="height: 68vh"
+    >
+      <q-carousel-slide :name="1" img-src="https://wallpapercave.com/wp/wp6424372.jpg" />
+      <q-carousel-slide :name="2" img-src="https://wallpapercave.com/wp/wp3826695.jpg" />
+      <q-carousel-slide :name="3" img-src="https://wallpaperboat.com/wp-content/uploads/2020/12/18/64126/future-21.jpg" />
+      <q-carousel-slide :name="4" img-src="https://wallpaperforu.com/wp-content/uploads/2020/07/car-wallpaper-2007261221322.jpg" />
+    </q-carousel> 
+
+    <q-page-container class="q-pt-none bg-dark">
+      <router-view />
+    </q-page-container>    
+    
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title:'Spotify',
+    icon: 'headphones',
+    link:'https://www.spotify.com'
+  },
+
+  {
+    title: 'YouTube Channel',
+    icon: 'smart_display',
+    link: 'https://youtube.com'
+  },
+
+  {
+    title: 'Instagram',
+    icon: 'local_see',
+    link: 'https://instagram.com'
+  },
+
+  {
+    title: 'Facebook',
+    icon: 'public',
+    link: 'https://facebook.com'
+  },
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData,
+      slide:1
+    }
+  }
+}
+</script>
+
+<style>
+  *{
+    padding:0;
+    margin:0;
+    -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+  }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,136 @@
+<template>
+  <q-page style="height:auto" class="bg-dark">
+
+    <q-separator vertical inset />
+
+    <q-card class="flex justify-center wrap q-ma-xl q-pt-xl q-pb-xl q-row-gutter-xl bg-blue-grey-10">
+      <q-card class="my-card q-pa-md bg-deep-purple-10 text-white q-ma-md" >
+        <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card q-pa-md bg-deep-purple-10 text-white q-ma-md" >
+        <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card q-pa-md bg-deep-purple-10 text-white q-ma-md" >
+        <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+      </q-card>
+    </q-card>
+
+    <q-card class="flex justify-center wrap q-ma-xl q-pt-xl q-pb-xl q-row-gutter-xl bg-blue-grey-10">
+      <q-card class="my-card q-pa-md bg-deep-purple-10 text-white q-ma-md" >
+        <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card q-pa-md bg-deep-purple-10 text-white q-ma-md" >
+        <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+      </q-card>
+
+      <q-card class="my-card q-pa-md bg-deep-purple-10 text-white q-ma-md" >
+        <q-video src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </q-card-section>
+      </q-card>
+
+    </q-card>
+
+    <q-separator vertical inset />
+
+    <q-footer bordered class="bg-blue-grey-10 text-primary">
+        <q-tabs no-caps active-color="floralwhite" indicator-color="transparent" class="text-white" v-model="tab">
+          <q-tab name="images" label="Images" />
+          <q-tab name="videos" label="Videos" />
+          <q-tab name="articles" label="Articles" />
+        </q-tabs>
+    </q-footer>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+  .my-card {
+    width: 100%;
+    max-width: 300px;
+    -webkit-transform: scale(1);
+	  transform: scale(1);
+	  -webkit-transition: .3s ease-in-out;
+	  transition: .3s ease-in-out;
+  }
+
+  .my-card:hover{
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+    cursor:pointer;
+  }
+
+
+</style>
